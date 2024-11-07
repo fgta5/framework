ee914110bc12220de8911f62eb0d00887000886b
agungdhewe <agung_dhewe@yahoo.com>
2024-11-07 09:11:10 +0700

    dev 20241107 0911

--- src/IModulePage.php
+++ src/IModulePage.php
@@ -1,6 +1,6 @@
 <?php namespace Fgta5\Framework;
 
 interface IModulePage {
-	public function LoadPage(array $params) : void;
+	public function LoadPage(string $requestedPage, array $params) : void;
 	public static function GetModulePagePath(string $modulePageClass) : string;
 }
